ff46c75ba1eeb8dd90d63683e817471e16a5b40c
JohnMac <jrivard23@outlook.com>
2019-12-04 15:41:50 -0600

    Finished mobile design

--- js/main.js
+++ js/main.js
@@ -5,16 +5,12 @@ console.log("Main.js Loaded");
 //
 var projectBoxes = document.getElementsByClassName("project-box");
 // projectBoxes[0] is the spotlight
-//Expand Divs
-var expands = new Array();
 //Buttons
 var moreButtons = new Array();
-//Get Expands and Buttons
+//Get Expand Buttons
 for(var i = 1; i < projectBoxes.length; i++){
     for(var x = 0; x < projectBoxes[i].childNodes.length; x++){
-        if(projectBoxes[i].childNodes[x].className == "expand"){
-            expands.push(projectBoxes[i].childNodes[x]);
-        }else if(projectBoxes[i].childNodes[x].textContent == "See More"){
+        if(projectBoxes[i].childNodes[x].textContent == "Show More"){
             moreButtons.push(projectBoxes[i].childNodes[x]);
         }
     }
@@ -22,22 +18,25 @@ for(var i = 1; i < projectBoxes.length; i++){
 
 //Set onclick for the buttons
 for(var i = 0; i < moreButtons.length; i++){
-    var num = i;
+    //Create function
     var func = function(){
-        if(this.textContent == "See More"){
+        //If the button is show more
+        if(this.textContent == "Show More"){
+            //Find and set the expand child to flex
             for(var x = 0; x < this.parentElement.childNodes.length; x++){
                 if(this.parentElement.childNodes[x].className == "expand"){
                     this.parentElement.childNodes[x].style.display = "flex";
                 }
             }
-            this.innerHTML = "See Less";
+            this.innerHTML = "Show Less";
         }else {
+            //Find and set the expand child to none
             for(var x = 0; x < this.parentElement.childNodes.length; x++){
                 if(this.parentElement.childNodes[x].className == "expand"){
                     this.parentElement.childNodes[x].style.display = "none";
                 }
             }
-            this.innerHTML = "See More";
+            this.innerHTML = "Show More";
         }
     };
     moreButtons[i].onclick = func;
